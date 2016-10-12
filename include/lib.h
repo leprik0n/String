e8ba9bc58efda7ed2312c700e371e5434ac3df29
leprik0n <diablo_soft@mail.ru>
2016-10-12 22:54:52 +0400

    Update lib.h

--- include/lib.h
+++ include/lib.h
@@ -1,1 +1,7 @@
+#pragma once
 
+int len(const char* s);
+int cmp(const char* lhs,const char* rhs);
+void s_copy(char* lhs,const char* rhs);
+void s_cat(char*& lhs,const char* rhs);
+void s_c(char*& lhs, const char rhs);
